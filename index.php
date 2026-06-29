25ea5f497315a3db093060f6247d3805a04d5ea1
Syllvvie <0sylv1eexe@gmail.com>
2026-06-29 19:05:07 -0300

    Adicionando CSS pra ficar bonito

--- index.php
+++ index.php
@@ -11,26 +11,32 @@ $tarefas = $todoList->listar();
     <head>
         <title>Lista de Tarefas</title>
         <script src="https://kit.fontawesome.com/22fd3c2633.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <form method="POST" action="criar.php">
-        <input type="text" name="titulo" placeholder="Digite a tarefa...">
-        <button type="submit">Criar</button>
-        </form>
-        <table style="border-collapse: collapse;">
-            <?php foreach ($tarefas as $t) { ?>
-                <tr style="border-bottom: 1px solid #e0e0e0;">
-                    <td><a href="concluir.php?id=<?= $t['id'] ?>">
-                        <?php if ($t['concluida']): ?>
-                            <i class="fa-solid fa-square-check"></i>
-                        <?php else: ?>
-                            <i class="fa-regular fa-square"></i>
-                        <?php endif; ?>
-                    </a></td>
-                    <td><span <?= $t['concluida'] ? ' style="text-decoration: line-through;"' : '' ?>><?= $t['titulo'] ?></span></td>
-                    <td><a href="excluir.php?id=<?= $t['id'] ?>"><i class="fa-solid fa-x" style="color: rgb(255, 10, 10);"></i></a></td>
-                </tr>
-            <?php } ?>
-        </table>
+        <div class="container">
+            <div class="titulo">To do List</div>
+
+            <form class="form-criar" method="POST" action="criar.php">
+                <input type="text" name="titulo" placeholder="Digite a tarefa...">
+                <button type="submit">Criar</button>
+            </form>
+
+            <table class="lista">
+                <?php foreach ($tarefas as $t) { ?>
+                    <tr class="tarefa">
+                        <td><a href="concluir.php?id=<?= $t['id'] ?>">
+                            <?php if ($t['concluida']): ?>
+                                <i class="fa-solid fa-square-check"></i>
+                            <?php else: ?>
+                                <i class="fa-regular fa-square"></i>
+                            <?php endif; ?>
+                        </a></td>
+                        <td><span <?= $t['concluida'] ? ' style="text-decoration: line-through;"' : '' ?>><?= $t['titulo'] ?></span></td>
+                        <td><a href="excluir.php?id=<?= $t['id'] ?>"><i class="fa-solid fa-x" style="color: rgb(255, 10, 10);"></i></a></td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
     </body>
 </html>
