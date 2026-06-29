3d4f763457b95954f9aa7c2c1d6320c2d4b1d33a
Syllvvie <0sylv1eexe@gmail.com>
2026-06-29 00:52:43 -0300

    Criando conexao com o banco de dados MySQL

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "todolist";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão bem-sucedida!";
+} catch(PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
